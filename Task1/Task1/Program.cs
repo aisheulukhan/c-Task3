c1d0f734992c4b3643faa15c01f1257be5fbfd4d
tu7hnsxs1 <tu7hnsxs1@code.edu.az>
2022-03-10 09:47:20 +0400

    Added

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 467, 64, -2, 6, 46, 28, 9, 364, };
+            
+            Swap(arr);
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        static void Swap(int[] arr)
+        {
+
+            int min = 0;
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[min] > arr[i])
+                    min = i;
+                if (arr[max] < arr[i])
+                    max = i;
+              
+            }
+            arr[min] = arr[max] + arr[min];
+            arr[max] = arr[min] - arr[max];
+            arr[min] = arr[min] - arr[max];
+
+        }
+
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 467, 64, -2, 6, 46, -28, 9, 364, };
+            NegativePotive(ref arr);
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        static void NegativePotive(ref int [] arr)
+        {
+            for (int i =0 ; i < arr.Length; i++)
+            {
+                if (arr[i]<0)
+                {
+                    arr[i] = arr[i] * -1;
+                }
+            }
+        }
+    }
+}
